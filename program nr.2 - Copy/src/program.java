caf678079449eec23ba3c929c0c82402c54debd0
Hristo44 <154980885+Hristo44@users.noreply.github.com>
2024-01-02 17:00:48 +0100

    Add files via upload
    this is the first project I have sent to discord on my own so apologies for any confusion or annoyance.
    this project shows how you can change "a" from being "bread" and "b" being "nutella" to the opposite (a being nutella and b being bread)

---
+++ program nr.2 - Copy/src/program.java
@@ -0,0 +1,20 @@
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		String a = "bread";
+		String b = "nutella";
+		String temp;//null
+		
+		temp = a;
+		a=b;
+		b=temp;
+		
+		
+		System.out.println("a: " +a);
+		System.out.println("b: " +b);
+
+	}
+
+}
